04b8e059cf7ac3181288102e455423aa65af375d
Hidayadie <hissats234@gmail.com>
2024-07-24 01:45:20 +0700

    sv

---
+++ Program UAS/Program UAS/Tampilan/EditOrang.cs
@@ -0,0 +1,31 @@
+﻿namespace Program_UAS;
+
+public class EditOrang : Menu
+{
+    public EditOrang()
+    {
+
+    }
+
+    public override void Tampilkan()
+    {
+        Console.Clear();
+        CetakAtas("*");
+        CetakSamping(1);
+        Console.SetCursorPosition(25 - (UkurString("Data Pribadi") / 2), 1);
+        Console.Write("Data Pribadi");
+
+        CetakSamping(2);
+        CetakSamping(3);
+        Console.Write("Nama: ");
+        CetakSamping(4);
+        Console.Write("Nomor Induk: ");
+        CetakSamping(5);
+        Console.Write("Jenis Kelamin: ");
+        CetakSamping(6);
+        Console.Write("Nama: ");
+        CetakSamping(7);
+        CetakSamping(8);
+        CetakSamping(9);
+    }
+}
